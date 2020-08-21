29eee7668102db7088f0737f15fa7d96a2ef668d
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-08-20 22:10:05 -0700

    Create tls.sh

---
+++ scripts/plg/tls/tls.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+function getProperty {
+    PROP_KEY=$1
+    PROP_VALUE=`printenv | grep "$PROP_KEY" | cut -d'=' -f2-`
+    echo $PROP_VALUE
+}
+
+function setPluginConfig {
+    pscm=$(getProperty "plg.tlc.scm")
+    phst=$(getProperty "plg.tlc.hst")
+    pspt=$(getProperty "plg.tls.prt")
+    ppxy=$(getProperty "plg.tls.pxy")
+    plpt=$(getProperty "plg.tls.lpt")
+    
+    sed -i "s|{EC_TLS_SCHEMA}|$pscm|g" ~/.ec/plg/tls/plugins.yml
+    sed -i "s|{EC_TLS_HOSTNAME}|$phst|g" ~/.ec/plg/tls/plugins.yml
+    sed -i "s|{EC_TLS_PORT}|$pspt|g" ~/.ec/plg/tls/plugins.yml
+    sed -i "s|{EC_TLS_PXY}|$ppxy|g" ~/.ec/plg/tls/plugins.yml
+    sed -i "s|{EC_TLS_RPT}|$plpt|g" ~/.ec/plg/tls/plugins.yml
+}
+
+setPluginConfig
+cp ~/.ec/plg/tls/plugins.yml ~/
