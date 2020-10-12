7d0fc2350fca63b0ad045c1ce73dbb1c2c868549
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-12 10:22:47 -0700

    Update tls.sh

--- scripts/plg/tls/tls.sh
+++ scripts/plg/tls/tls.sh
@@ -3,7 +3,7 @@
 function getProperty {
     PROP_KEY=$1
     PROP_VALUE=`printenv | grep "$PROP_KEY" | cut -d'=' -f2-`
-    PROP_VALUE=$(sed "s|$PROP_VALUE|\"|g")
+    PROP_VALUE=$(echo $PROP_VALUE | sed 's|\"||g')
     echo $PROP_VALUE
 }
 
