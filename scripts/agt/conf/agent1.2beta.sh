41b1d1ea82604150d6ed3887ee52b53a24f3ee96
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-10-31 10:35:41 -0700

    Update agent1.2beta.sh

--- scripts/agt/conf/agent1.2beta.sh
+++ scripts/agt/conf/agent1.2beta.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
-#
-#  Copyright (c) 2020 General Electric Company. All rights reserved.
-#
-#  The copyright to the computer software herein is the property of
-#  General Electric Company. The software may be used and/or copied only
-#  with the written permission of General Electric Company or in accordance
-#  with the terms and conditions stipulated in the agreement/contract
-#  under which the software has been supplied.
-#
-#  author: apolo.yasuda@ge.com
-#
-
-case $EC_AUTH_VALIDATE in
-  oaep)
-    echo "launch oauth with oaep"
-    cat ~/.ec/conf/oauth_oaep.yaml
-    ;;
-  oidc)
-    echo "launch oauth with oidc"
-    cat ~/.ec/conf/oauth_oidc.yaml
-    ;;
-  *)
-    ;;
-esac
 
 echo hello agent1.2beta
-ls -al && ls -al ~/.ec && ls -al ~/.ec/conf
 #agent -cfg .ec/conf/${mod}.yml
-agent -ver
+agent $@
