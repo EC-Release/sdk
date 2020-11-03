f20392e87b466e9d190f7d8304703501dfb0a358
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-03 13:56:32 -0800

    Update ng.sh

--- scripts/api/ng-webui/ng.sh
+++ scripts/api/ng-webui/ng.sh
@@ -4,3 +4,10 @@ if [ ! -f "~/.ec/agent" ]; then
 fi
 
 source <(wget -O - https://ec-release.github.io/sdk/scripts/api/ng-webui/ng_conf.sh)
+
+if [ "$#" -ne 1 ]; then
+    agent -cfg ~/.ec/api/conf/api.yaml
+    exit 0
+fi
+
+agent $@
