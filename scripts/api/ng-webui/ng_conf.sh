8fbd904c8cbdd2c2cc76120e2c056590ff2b5870
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-03 11:49:21 -0800

    Update ng_conf.sh

--- scripts/api/ng-webui/ng_conf.sh
+++ scripts/api/ng-webui/ng_conf.sh
@@ -3,4 +3,4 @@ mkdir -p ~/.ec/api/ng-webui/
 wget -q --show-progress -O ~/.ec/api/ng-webui/ng.tar.gz https://github.com/EC-Release/ng-webui/archive/main.tar.gz
 tar -xzf ~/.ec/api/ng-webui/ng.tar.gz --strip 1 -C ~/.ec/api/ng-webui
 rm ~/.ec/api/ng-webui/ng.tar.gz
-ls -al ~/.ec/api/ng-webui ~/.ec/api/ng-webui
+ls -al ~/.ec/api/ng-webui ~/.ec/api
