165c306d5bac5b50e5fa443ac98384613dd0e825
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-10 09:59:35 -0800

    Update run.sh

--- scripts/service1.x/run.sh
+++ scripts/service1.x/run.sh
@@ -4,22 +4,23 @@ tar -zxf ./temp.tar.gz
 rm temp.tar.gz
 cd temp
 
-#auth api read certs directly
 export EC_PUB_KEY="$(cat service.cer)"
 export EC_PRVT_KEY="$(cat service.key)"
 
-# begin test keypair
+
+printf "begin test keypair"
 source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.2beta.linux64.txt) 
 export EC_PPS=${EC_PRVT_ADM}
 export EC_PPS=$(agent -hsh)
 agent -pvd -pvk $(cat service.key|base64 -w0)
-# end test keypair
+printf "end test keypair"
 
-# begin auth-api replacement
+printf "begin auth-api replacement"
 wget -q --show-progress https://github.com/EC-Release/auth-api/raw/v1beta/dist/api/api_linux.tar.gz
 tar -xzf api_linux.tar.gz
 chmod -x api_linux
-# end auth-api replacement
+printf "end auth-api replacement"
+
 
 ls -la $(pwd)
 eval "sed -i -e 's#{HOST}#${APP}-${ENV}.${HOST}#g' ./assets/swagger.json"
