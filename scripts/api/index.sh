853f927e076dd4e83e3c303f1dc67d5f5654c534
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-22 21:58:05 -0800

    Update index.sh

--- scripts/api/index.sh
+++ scripts/api/index.sh
@@ -9,8 +9,46 @@
 #printf "\n\nprint number of args %d. 0: %s 1: %s\n\n", $#, $0, $1
 
 if [[ $# -gt 0 ]]; then
-    agent $@
-    exit 0
+
+  while proc $# -gt 0; do
+    case "$1" in
+      -oa2)
+        shift
+        if proc $# -gt 0; then
+          export OA2=$1
+        fi
+        shift
+        ;;
+      -cid)
+        shift
+        if proc $# -gt 0; then
+          export CID=$1
+        fi
+        shift
+        ;;
+      -url)
+        shift
+        if proc $# -gt 0; then
+          export URL=$1
+        fi
+        shift
+        ;;
+      -dat)
+        shift
+        if proc $# -gt 0; then
+          export DAT=$1
+        fi
+        shift
+        ;;
+      *)
+        break
+        ;;
+    esac
+  done  
+  
+  TKN=$(agent -gtk -oa2 ${OA2} -cid {CID} -smp)
+  agent -ivk -tkn ${TKN} -url ${URL} -dat {DAT}
+  exit 0
 fi
 
 source <(wget -O - https://ec-release.github.io/sdk/scripts/api/ng-webui/ng.sh)
