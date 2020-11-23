4b7263c1e7e1d2d8354890fb23fd805aaa1bc224
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-22 22:43:43 -0800

    Update index.sh

--- scripts/api/index.sh
+++ scripts/api/index.sh
@@ -49,7 +49,7 @@ if [[ $# -gt 0 ]]; then
   
   printf "\noa2: %s, cid: %s, url: %s, dat: %s\n\n",$OA2,$CID,$URL,$DAT
   
-  agent -gtk -oa2 $OA2 -cid $CID -smp | { read TKN; echo TKN=$TKN; }
+  TKN=$(agent -gtk -oa2 $OA2 -cid $CID -smp)
   agent -ivk -tkn ${TKN} -url ${URL} -dat ${DAT}
   exit 0
 fi
