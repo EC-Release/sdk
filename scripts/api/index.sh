3b6b57e7d2dfe30f3fc179a9386aedc46c139b48
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-22 20:11:56 -0800

    Update index.sh

--- scripts/api/index.sh
+++ scripts/api/index.sh
@@ -3,6 +3,7 @@
 {
     agent -ver
 } || {
+    print "\ncontinue agent installation\n\n"
     source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.2beta.linux64_conf.txt)
 }
 printf "\n\nprint number of args %d. 0: %s 1: %s\n\n", $#, $0, $1
