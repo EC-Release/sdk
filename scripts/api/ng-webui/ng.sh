4ba29f2ee4c03e8989e7696c29747d5ad95a0485
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-22 19:28:25 -0800

    Update ng.sh

--- scripts/api/ng-webui/ng.sh
+++ scripts/api/ng-webui/ng.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ ! -f "~/.ec/agent" ]; then
-    source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.2beta.linux64_conf.txt)
-fi
 
 source <(wget -O - https://ec-release.github.io/sdk/scripts/api/ng-webui/ng_conf.sh)
 
