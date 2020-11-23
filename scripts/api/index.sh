8896e2e648196745221f3ef30d1a29801d763476
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-22 20:08:51 -0800

    Update index.sh

--- scripts/api/index.sh
+++ scripts/api/index.sh
@@ -5,7 +5,7 @@
 } || {
     source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.2beta.linux64_conf.txt)
 }
-#printf "\n\nprint args %d. 0: %s 1: %s\n\n", $#, $0, $1
+printf "\n\nprint number of args %d. 0: %s 1: %s\n\n", $#, $0, $1
 
 if [[ $# -gt 1 ]]; then
     agent $@
