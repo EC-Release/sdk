d57fdbf2dcba115d4d518add5771ae7c3afe33a4
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-11-22 19:28:52 -0800

    Update index.sh

--- scripts/api/index.sh
+++ scripts/api/index.sh
@@ -1,3 +1,12 @@
-
 #!/bin/bash
-source <(wget -O - https://ec-release.github.io/sdk/scripts/api/ng-webui/ng.sh) $@
+
+if [ ! -f "~/.ec/agent" ]; then
+    source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.2beta.linux64_conf.txt)
+fi
+
+if [[ $# -ne 0 ]]; then
+    agent $@
+    exit 0
+fi
+
+source <(wget -O - https://ec-release.github.io/sdk/scripts/api/ng-webui/ng.sh)
