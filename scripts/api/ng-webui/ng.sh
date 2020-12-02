a1df3594a58b3605a9669767cde766ef262d3a93
ayasuda-ge <70172309+ayasuda-ge@users.noreply.github.com>
2020-12-02 10:57:23 -0800

    Update ng.sh

--- scripts/api/ng-webui/ng.sh
+++ scripts/api/ng-webui/ng.sh
@@ -5,6 +5,7 @@ source <(wget -O - https://ec-release.github.io/sdk/scripts/api/ng-webui/ng_conf
 # refresh the hash
 export EC_PPS=${CA_PPRS}
 export EC_PPS=$(agent -hsh)
+printf "\n\nEC_PPS: %s\n\n" $EC_PPS
 # PORT indicating a custom environment
 if [[ ! -z "${PORT}" ]]; then
   EC_PORT=:$PORT
