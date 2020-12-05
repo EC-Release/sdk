1dc2258a2bcefbc35e4ecd2ab869c768ee922c34
ayasuda-ge <70172309+ayasuda-ge@users.noreply.github.com>
2020-12-04 23:47:05 -0800

    Update index.sh

--- scripts/api/index.sh
+++ scripts/api/index.sh
@@ -4,7 +4,7 @@
     agent -ver
 } || {
     printf "\nmissing agent. begin agent installation\n\n"
-    source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.2beta.linux64.txt)
+    source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64_conf.txt)
 }
 
 if [[ $# -gt 1 ]]; then
