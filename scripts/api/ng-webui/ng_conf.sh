9e072c1b205bc2a14bb7a365500e1f287f731046
ayasuda-ge <70172309+ayasuda-ge@users.noreply.github.com>
2020-12-07 11:31:39 -0800

    Update ng_conf.sh

--- scripts/api/ng-webui/ng_conf.sh
+++ scripts/api/ng-webui/ng_conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.ec/api/webui-assets/ ~/.ec/api/conf
-wget -q --show-progress -O ~/.ec/api/webui-assets/v1.2beta.tar.gz https://github.com/EC-Release/ng-webui/archive/v1.2beta.tar.gz
+wget -q --show-progress -O ~/.ec/api/webui-assets/v1.2beta.tar.gz https://github.com/EC-Release/ng-portal/archive/v1.2beta.tar.gz
 tar -xzf ~/.ec/api/webui-assets/v1.2beta.tar.gz --strip 1 -C ~/.ec/api/webui-assets
 rm ~/.ec/api/webui-assets/v1.2beta.tar.gz
 wget -q --show-progress -O ~/.ec/api/conf/api.yaml https://ec-release.github.io/sdk/scripts/api/conf/api.yaml
