b1365d9f57bcff411cad7dbfd840e87b8940dc2d
ayasuda-ge <70172309+ayasuda-ge@users.noreply.github.com>
2020-12-08 08:50:50 -0800

    Update index.sh

--- scripts/api/index.sh
+++ scripts/api/index.sh
@@ -14,55 +14,55 @@ if [[ $# -gt 1 ]]; then
       -oa2)
         shift
         if test $# -gt 0; then
-          export OA2=$1
+          export OA2="$1"
         fi
         shift
         ;;
       -cid)
         shift
         if test $# -gt 0; then
-          export CID=$1
+          export CID="$1"
         fi
         shift
         ;;
       -url)
         shift
         if test $# -gt 0; then
-          export URL=$1
+          export URL="$1"
         fi
         shift
         ;;
       -dat)
         shift
         if test $# -gt 0; then
-          export DAT=$1
+          export DAT="$1"
         fi
         shift
         ;;
       -mtd)
         shift
         if test $# -gt 0; then
-          export MTD=$1
+          export MTD="$1"
         fi
         shift
         ;;
       *)
-        printf "\nflag: %s", $1
+        printf "\nflag: %s", "$1"
         break
         ;;
     esac
   done  
   
-  printf "\n-oa2: %s, -cid: %s, -url: %s, -dat: %s\n\n" $OA2 $CID $URL $DAT
+  printf "\n-oa2: %s, -cid: %s, -url: %s, -dat: %s\n\n" "$OA2" "$CID" "$URL" "$DAT"
   
   if [[ -z "${EC_PPS}" ]]; then
     export EC_PPS=$CA_PPRS    
   fi
   
   export EC_PPS=$(agent -hsh -smp)
-  TKN=$(agent -gtk -oa2 $OA2 -cid $CID -smp)
+  TKN=$(agent -gtk -oa2 "$OA2" -cid "$CID" -smp)
   printf "\n bearer token: %s\n\n" $TKN
-  agent -ivk -tkn ${TKN} -url ${URL} -dat ${DAT} -mtd ${MTD}
+  agent -ivk -tkn "${TKN}" -url "${URL}" -dat "${DAT}" -mtd "${MTD}"
   exit 0
 fi
 
