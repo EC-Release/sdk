d8b2dfcf96d810a325183725181c286c31f65637
ramaraosrikakulapu <38732583+ramaraosrikakulapu@users.noreply.github.com>
2021-01-08 12:46:35 -0500

    Update ec-nginx.sh

--- scripts/loadbalancer/conf/ec-nginx.sh
+++ scripts/loadbalancer/conf/ec-nginx.sh
@@ -1,3 +1,10 @@
-!#/bin/bash
+#!/bin/bash
+
+{
+    agent -ver
+} || {
+    printf "\nmissing agent. begin agent installation\n\n"
+    source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64_conf.txt)
+}
 
 sed /{server-block}/ec-nginx-server-block.conf
