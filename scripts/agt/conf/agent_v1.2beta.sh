c6c1627d8e147db9e98d2a5e051040df571b06a5
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2021-01-19 11:26:59 -0800

    Update agent_v1.2beta.sh

--- scripts/agt/conf/agent_v1.2beta.sh
+++ scripts/agt/conf/agent_v1.2beta.sh
@@ -3,5 +3,11 @@
 agent -cfg .ec/conf/${mod}.yml
 printf "flag %s presented.\n" "$@"
 '
+  
+if [[ -z "${EC_PPS}" ]]; then
+  export EC_PPS=$CA_PPRS    
+fi  
+  
+export EC_PPS=$(agent -hsh -smp)
 
 agent "$@"
