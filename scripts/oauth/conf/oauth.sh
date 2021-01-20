99704e27f16b0454e6fb6f8bb3b73cba2ebb53fb
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-19 19:27:39 -0800

    Update oauth.sh

--- scripts/oauth/conf/oauth.sh
+++ scripts/oauth/conf/oauth.sh
@@ -11,6 +11,13 @@
 #  author: apolo.yasuda@ge.com
 #
 
+{
+    agent -ver
+} || {
+    printf "\nmissing agent. begin agent installation\n\n"
+    source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64_conf.txt)
+}
+
 # PORT indicating a custom environment
 if [[ ! -z "${PORT}" ]]; then
   EC_PORT=:$PORT
