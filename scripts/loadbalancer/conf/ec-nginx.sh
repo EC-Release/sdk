21177d2ec51d67de926b16d0f0f1d549294b8aff
ramaraosrikakulapu <38732583+ramaraosrikakulapu@users.noreply.github.com>
2021-01-21 15:49:32 -0500

    Update ec-nginx.sh

--- scripts/loadbalancer/conf/ec-nginx.sh
+++ scripts/loadbalancer/conf/ec-nginx.sh
@@ -7,8 +7,10 @@
     source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64_conf.txt)
 }
 
+printf "\nexecuting the sed command\n"
 sed -i "s/{server-block}/$serverblock/g" ~/.ec/conf/lb/ec-nginx-server-block.conf
 
+printf "\ncat nginx-server-block.conf file\n"
 cat ~/.ec/conf/lb/ec-nginx-server-block.conf
 
 nginx -t -c ~/.ec/conf/lb/ec-nginx-server-block.conf
