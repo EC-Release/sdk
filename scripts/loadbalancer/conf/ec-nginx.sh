6223b5f4606933dc223d3afbe0190886c3c3159f
ramaraosrikakulapu <38732583+ramaraosrikakulapu@users.noreply.github.com>
2021-01-22 10:52:25 -0500

    Update ec-nginx.sh

--- scripts/loadbalancer/conf/ec-nginx.sh
+++ scripts/loadbalancer/conf/ec-nginx.sh
@@ -5,13 +5,11 @@ printf "\n serverblock env value: $serverblock\n"
 printf "\nnginx-server-block.conf file.........\n"
 cat ~/.ec/conf/lb/ec-nginx-server-block.conf
 
-upstreamblock=`echo ${serverblock} | tr '\n' "\\n"`
-printf "\nupstreamblock: $upstreamblock\n"
-
 printf "\nexecuting the sed command..............\n"
-sed -i "s/UPSTREAMBLOCK/$upstreamblock/g" ~/.ec/conf/lb/ec-nginx-server-block.conf
+ESCAPED=$(echo "${serverblock}" | sed '$!s@$@\\@g')
+sed "s/UPSTREAMBLOCK/${ESCAPED}/g" ec-nginx-server-block.conf > ec-nginx-server-block-updated.conf
 
-printf "\nnginx-server-block.conf file after sed.........\n"
-cat ~/.ec/conf/lb/ec-nginx-server-block.conf
+printf "\nnginx-server-block-updated.conf file after sed.........\n"
+cat ~/.ec/conf/lb/ec-nginx-server-block-updated.conf
 
-nginx -t -c ~/.ec/conf/lb/ec-nginx-server-block.conf
+nginx -t -c ~/.ec/conf/lb/ec-nginx-server-block-updated.conf
