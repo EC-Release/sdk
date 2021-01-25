aabb810beed2751cfcbe1bc45443f3fe776efe48
ramaraosrikakulapu <38732583+ramaraosrikakulapu@users.noreply.github.com>
2021-01-25 15:47:28 -0500

    Update ec-nginx.sh

--- scripts/loadbalancer/conf/ec-nginx.sh
+++ scripts/loadbalancer/conf/ec-nginx.sh
@@ -2,6 +2,13 @@
 
 printf "\n serverblock env value: $serverblock\n"
 
+for(i=0; i<$LBER_REPLICA_COUNT; i++) {
+    if my-app-agent-$i = $HOSTNAME
+    {
+        export CF_INSTANCE_INDEX=$i
+    }
+}
+
 ESCAPED=$(echo "${serverblock}" | sed '$!s@$@\\@g')
 sed "s/UPSTREAMBLOCK/${ESCAPED}/g" ~/.ec/conf/lb/ec-nginx-server-block.conf > ~/.ec/conf/lb/ec-nginx-server-block-updated.conf
 
