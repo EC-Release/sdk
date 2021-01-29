90def963da0fb61c8699c52a7c77967bf957be6b
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-28 20:15:02 -0800

    Update index.sh

--- scripts/api/index.sh
+++ scripts/api/index.sh
@@ -9,7 +9,9 @@
 
 mkdir -p ~/.ec/scripts
 wget -q --show-progress -O ~/.ec/scripts/executor.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/executor.sh
-chmod +x ~/.ec/scripts/executor.sh
+wget -q --show-progress -O ~/.ec/scripts/cli.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/cli.sh
+wget -q --show-progress -O ~/.ec/scripts/exec.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/exec.sh
+chmod +x ~/.ec/scripts/executor.sh ~/.ec/scripts/exec.sh ~/.ec/scripts/cli.sh
 ls -la ~/.ec/scripts
 
 if [[ $# -gt 1 ]]; then
