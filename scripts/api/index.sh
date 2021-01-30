6372f6283617337a9c719c2ca39f6639ebd90c01
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-30 10:23:34 -0800

    Update index.sh

--- scripts/api/index.sh
+++ scripts/api/index.sh
@@ -8,7 +8,7 @@
 }
 
 mkdir -p ~/.ec/scripts
-wget -q --show-progress -O ~/.ec/scripts/executor.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/executor.sh
+#wget -q --show-progress -O ~/.ec/scripts/executor.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/executor.sh
 wget -q --show-progress -O ~/.ec/scripts/cli.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/cli.sh
 wget -q --show-progress -O ~/.ec/scripts/exec.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/exec.sh
 chmod +x ~/.ec/scripts/executor.sh ~/.ec/scripts/exec.sh ~/.ec/scripts/cli.sh
