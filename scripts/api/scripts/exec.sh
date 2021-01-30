3db2b22613b3e1e5ca3ed9f2027569152ae17c06
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-30 11:48:11 -0800

    Update exec.sh

--- scripts/api/scripts/exec.sh
+++ scripts/api/scripts/exec.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#exec 0<&-
-
 #base64 | tr -d \\n
 stty rows 40 cols 120
 #/bin/bash -l
 
 wget -q --show-progress -O ~/.ec/scripts/exec-bootstrap.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/exec-bootstrap.sh
 chmod +x ~/.ec/scripts/exec-bootstrap.sh
-source ~/.ec/scripts/exec-bootstrap.sh "$@"
+source ~/.ec/scripts/exec-bootstrap.sh "$@" 0<&-
