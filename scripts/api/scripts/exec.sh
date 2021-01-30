a41e45b9128ce5a965aa4775448b9e240005f310
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-30 10:19:31 -0800

    Update exec.sh

--- scripts/api/scripts/exec.sh
+++ scripts/api/scripts/exec.sh
@@ -2,6 +2,53 @@
 
 #base64 | tr -d \\n
 stty rows 40 cols 120
-/bin/bash -l
+#/bin/bash -l
+
+if [[ $# -gt 1 ]]; then
+
+  while test $# -gt 1; do
+    case "$1" in
+      --cm)
+        shift
+        if test $# -gt 0; then
+          EXEC_GIT_COMMIT="$1"
+        fi
+        shift
+        ;;
+      --vd)
+        shift
+        if test $# -gt 0; then
+          EXEC_VENDOR="$1"
+        fi
+        shift
+        ;;
+      --fq)
+        shift
+        if test $# -gt 0; then
+          EXEC_FREQ="$1"
+        fi
+        shift
+        ;;
+      --ul)
+        shift
+        if test $# -gt 0; then
+          EXEC_URL="$1"
+        fi
+        shift
+        ;;
+      *)
+        printf "\nflag: %s", "$1"
+        break
+        ;;
+    esac
+  done  
+  
+  printf "\n--cm: %s, --vd: %s, --fq: %s, --ul: %s\n\n" "$EXEC_GIT_COMMIT" "$EXEC_VENDOR" "$EXEC_FREQ" "$EXEC_URL"
+  
+  TIME=$(date)
+  printf "\n\n local time: %s\n\n" "$TIME"
+ 
+  exit 0
+fi
 
 #exec task here  hint: with readyonly "<&-"
