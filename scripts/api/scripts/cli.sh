e8f64e21e5c1e0ba5645f4b46b29c2d88efb2224
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-30 20:35:17 -0800

    Update cli.sh

--- scripts/api/scripts/cli.sh
+++ scripts/api/scripts/cli.sh
@@ -3,7 +3,7 @@
 export TERM=xterm-color
 #base64 | tr -d \\n
 stty rows 40 cols 120
-print "\n\nhello"
+#print "\n\nhello"
 #/bin/bash -l
 
 wget -q --show-progress -O ~/.ec/scripts/cli-bootstrap.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/cli-bootstrap.sh
