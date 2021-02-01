9a4a5fa1fd47c85c2a526d0838310e450852eb6d
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-01 12:14:31 -0800

    Update cli.sh

--- scripts/api/scripts/cli.sh
+++ scripts/api/scripts/cli.sh
@@ -7,7 +7,6 @@ stty rows 40 cols 120
 #/bin/bash -l
 
 wget -q --show-progress -O ~/.ec/scripts/cli-bootstrap.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/cli-bootstrap.sh
-wget -q --show-progress -O ~/.ec/scripts/cli-bootstrap-one.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/cli-bootstrap-one.sh
 chmod +x ~/.ec/scripts/cli-bootstrap.sh
 
 exec env --ignore-environment /root/.ec/scripts/cli-bootstrap.sh "$@"
