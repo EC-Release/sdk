ad0e04ae1cf22461a15e5340598fa4568af848be
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-01 08:09:27 -0800

    Update cli-bootstrap.sh

--- scripts/api/scripts/cli-bootstrap.sh
+++ scripts/api/scripts/cli-bootstrap.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #rm -Rf ~/.ec/script/cli-bootstrap.sh ~/.ec/script/exec-bootstrap.sh
-exec env --ignore-environment /bin/bash -l
-
-source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.linux64.txt) -ver
+exec env --ignore-environment source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.linux64.txt) -ver
