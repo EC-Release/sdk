81b88763016e55f9aa3d530eb2ec3988877782f3
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-01 11:59:50 -0800

    Update cli-bootstrap.sh

--- scripts/api/scripts/cli-bootstrap.sh
+++ scripts/api/scripts/cli-bootstrap.sh
@@ -2,4 +2,4 @@
 #rm -Rf ~/.ec/script/cli-bootstrap.sh ~/.ec/script/exec-bootstrap.sh
 #wget -O ./v1.2beta.linux64 https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64.txt
 
-bash -l && export "PATH=$PATH:/root/.ec"
+bash -l ls -al
