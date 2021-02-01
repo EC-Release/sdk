a726392857db5c81909956c63b8b145308906bb6
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-01 12:57:08 -0800

    Update cli-bootstrap.sh

--- scripts/api/scripts/cli-bootstrap.sh
+++ scripts/api/scripts/cli-bootstrap.sh
@@ -4,6 +4,5 @@
 #wget -q --show-progress -O ~/.ec/scripts/cli-bootstrap-one.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/cli-bootstrap-one.sh
 #chmod +x ~/.ec/scripts/cli-bootstrap-one.sh
 
-bash -c export PATH=$PATH:/root/.ec
-
+bash -l
 #~/.ec/scripts/cli-bootstrap-one.sh
