c2d947fb19ce6e7eff9f50126d6779ba81ad77f2
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-01 13:10:33 -0800

    Update cli.sh

--- scripts/api/scripts/cli.sh
+++ scripts/api/scripts/cli.sh
@@ -6,6 +6,8 @@ stty rows 40 cols 120
 #print "\n\nhello"
 #/bin/bash -l
 
+ln -s ~/.ec/agt/bin/${AGENT_REV_LOCAL} /bin/agent
+
 wget -q --show-progress -O ~/.ec/scripts/cli-bootstrap.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/cli-bootstrap.sh
 chmod +x ~/.ec/scripts/cli-bootstrap.sh
 
