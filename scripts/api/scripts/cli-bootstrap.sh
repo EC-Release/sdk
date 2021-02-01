afa86efd8d7bd8a53fab180ec37ea115e195d45b
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-01 12:36:59 -0800

    Update cli-bootstrap.sh

--- scripts/api/scripts/cli-bootstrap.sh
+++ scripts/api/scripts/cli-bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #rm -Rf ~/.ec/script/cli-bootstrap.sh ~/.ec/script/exec-bootstrap.sh
 #wget -O ./v1.2beta.linux64 https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64.txt
-wget -q --show-progress -O ~/.ec/scripts/cli-bootstrap-one.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/cli-bootstrap-one.sh
-chmod +x ~/.ec/scripts/cli-bootstrap-one.sh
+#wget -q --show-progress -O ~/.ec/scripts/cli-bootstrap-one.sh https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/scripts/cli-bootstrap-one.sh
+#chmod +x ~/.ec/scripts/cli-bootstrap-one.sh
 
-bash -l ~/.ec/scripts/cli-bootstrap-one.sh
+bash -l set PATH=$PATH:/root/.ec
+
+#~/.ec/scripts/cli-bootstrap-one.sh
