8aa21be7529818fd24c81eb54cc37f4b325e3ee5
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-31 23:19:56 -0800

    Update exec-bootstrap.sh

--- scripts/api/scripts/exec-bootstrap.sh
+++ scripts/api/scripts/exec-bootstrap.sh
@@ -44,8 +44,10 @@ if [[ $# -gt 1 ]]; then
   TIME=$(date)
   printf "\n\n local time: %s\n\n" "$TIME"
  
-  
-  /bin/bash -l
+  rm -Rf ~/.ec/scripts
+  exec env --ignore-environment /bin/bash -l
+
+  #/bin/bash -l
   #vi
   #exit 0
 fi
