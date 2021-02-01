64be9eb791e831fedc470afa6c28da8007555ee0
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-01-31 23:59:22 -0800

    Update cli-bootstrap.sh

--- scripts/api/scripts/cli-bootstrap.sh
+++ scripts/api/scripts/cli-bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -Rf ~/.ec/script/cli-bootstrap.sh ~/.ec/script/exec-bootstrap.sh
+#rm -Rf ~/.ec/script/cli-bootstrap.sh ~/.ec/script/exec-bootstrap.sh
 exec env --ignore-environment /bin/bash -l
