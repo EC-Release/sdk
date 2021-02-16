7428d12e9f4c17f09339fc036030e37383f10e5d
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-16 15:00:14 -0800

    Update agent_v1.2beta.sh

--- scripts/agt/conf/agent_v1.2beta.sh
+++ scripts/agt/conf/agent_v1.2beta.sh
@@ -6,8 +6,33 @@ export EC_PPS=$CA_PPRS
 [[ ! -z "${EC_PPS}" ]] && echo "Not empty" || echo "Empty"
 '
 
+#function outputYaml {
+#    PROP_KEY=$1
+#    PROP_VALUE=`printenv | grep "$PROP_KEY" | cut -d'=' -f2-`
+#    echo $PROP_VALUE
+#}
+
 if [[ ! -z "${EC_PPS}" ]]; then
   export EC_PPS=$(agent -hsh -smp)
-fi  
+fi
+
+if [[ $* == *-cvt* ]]; then
+  while test $# -gt 1; do
+    case "$1" in
+      -mod)
+        shift
+        if test $# -gt 0; then
+          print "mod=%s" "$1" > /tmp/cvt.yaml
+        fi
+        shift
+        ;;
+      *)
+        printf "\nflag: %s", "$1"
+        break
+        ;;
+    esac
+  done
+  exit 0
+fi
   
 agent "$@"
