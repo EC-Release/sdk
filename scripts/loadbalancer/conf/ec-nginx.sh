bc5224f2be70630d71fceb8b1dc0d31bf4570354
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-02-19 12:08:59 -0800

    create working block for nginx conf

--- scripts/loadbalancer/conf/ec-nginx.sh
+++ scripts/loadbalancer/conf/ec-nginx.sh
@@ -7,7 +7,13 @@
     source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64_conf.txt)
 }
 
+# refer serverblock to https://github.com/EC-Release/oci/blob/fd2ad16359a79f0436654d3e9a56fc327ed709db/k8s/agent%2Bhelper/templates/_loadbalancer.tpl
+### begin of ngix config
+
+### end of nginx config
+
 ESCAPED=$(echo "${serverblock}" | sed '$!s@$@\\@g')
+
 sed "s/UPSTREAMBLOCK/${ESCAPED}/g" ~/.ec/conf/lb/ec-nginx-server-block.conf > ~/.ec/conf/lb/ec-nginx-server-block-updated.conf
 
 cp ~/.ec/conf/lb/ec-nginx-server-block-updated.conf /etc/nginx/conf.d/ec-nginx-server-block-updated.conf
