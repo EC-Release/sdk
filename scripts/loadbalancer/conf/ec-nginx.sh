261cc86ac0506a73fbc1b1d433e2a74b477c2e6e
ramaraosrikakulapu <38732583+ramaraosrikakulapu@users.noreply.github.com>
2021-02-22 02:59:48 -0500

    Update ec-nginx.sh

--- scripts/loadbalancer/conf/ec-nginx.sh
+++ scripts/loadbalancer/conf/ec-nginx.sh
@@ -10,8 +10,6 @@
 ESCAPED=$(echo "${serverblock}" | sed '$!s@$@\\@g')
 sed "s/UPSTREAMBLOCK/${ESCAPED}/g" ~/.ec/conf/lb/ec-nginx-server-block.conf > ~/.ec/conf/lb/ec-nginx-server-block-updated.conf
 
-printf "\nCopying nginx conf files..\n\n"
-pwd && ls -al && ls -al ~/.ec/conf/lb/
 cp -f ~/.ec/conf/lb/nginx.conf /etc/nginx/nginx.conf
 cp ~/.ec/conf/lb/ec-nginx-server-block-updated.conf /etc/nginx/conf.d/ec-nginx-server-block-updated.conf
 
