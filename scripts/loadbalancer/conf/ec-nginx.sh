15d875e40074b030d620b2df3af7304cf8a66b16
ramaraosrikakulapu <38732583+ramaraosrikakulapu@users.noreply.github.com>
2021-02-22 02:28:32 -0500

    Update ec-nginx.sh

--- scripts/loadbalancer/conf/ec-nginx.sh
+++ scripts/loadbalancer/conf/ec-nginx.sh
@@ -10,7 +10,7 @@
 ESCAPED=$(echo "${serverblock}" | sed '$!s@$@\\@g')
 sed "s/UPSTREAMBLOCK/${ESCAPED}/g" ~/.ec/conf/lb/ec-nginx-server-block.conf > ~/.ec/conf/lb/ec-nginx-server-block-updated.conf
 
-cp ~/.ec/conf/lb/ec-nginx-server-block-updated.conf /etc/nginx/conf.d/ec-nginx-server-block-updated.conf
 cp -f ~/.ec/conf/lb/nginx.conf /etc/nginx/nginx.conf
+cp ~/.ec/conf/lb/ec-nginx-server-block-updated.conf /etc/nginx/conf.d/ec-nginx-server-block-updated.conf
 
 nginx -g 'daemon off;'
