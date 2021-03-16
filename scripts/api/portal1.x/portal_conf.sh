479728aaf304e5ac120783c83351b26012f9f3c4
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-03-16 12:21:36 -0700

    Update portal_conf.sh

--- scripts/api/portal1.x/portal_conf.sh
+++ scripts/api/portal1.x/portal_conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir -p ~/.ec/api/webui-assets/ ~/.ec/api/conf
-wget -q --show-progress -O ~/.ec/api/webui-assets/v1.1beta.tar.gz https://github.com/EC-Release/web-ui/archive/v1.1beta.tar.gz
-tar -xzf ~/.ec/api/webui-assets/v1.1beta.tar.gz --strip 1 -C ~/.ec/api/webui-assets
-rm ~/.ec/api/webui-assets/v1.1beta.tar.gz
+mkdir -p ~/.ec/api/portal1.x/ ~/.ec/api/conf
+wget -q --show-progress -O ~/.ec/api/portal1.x/v1.1beta.tar.gz https://github.com/EC-Release/web-ui/archive/v1.1beta.tar.gz
+tar -xzf ~/.ec/api/portal1.x/v1.1beta.tar.gz --strip 1 -C ~/.ec/api/portal1.x
+rm ~/.ec/api/portal1.x/v1.1beta.tar.gz
 wget -q --show-progress -O ~/.ec/api/conf/api.yaml https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/conf/api.yaml
-ls -al ~/.ec/api/webui-assets ~/.ec/api ~/.ec/api/conf/
+ls -al ~/.ec/api/portal1.x ~/.ec/api ~/.ec/api/conf/
