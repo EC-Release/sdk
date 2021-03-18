c3b7219b317fa58e4f4784c60831637319891934
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-03-18 08:10:12 -0700

    Update portal_conf.sh

--- scripts/api/portal1.x/portal_conf.sh
+++ scripts/api/portal1.x/portal_conf.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 mkdir -p ~/.ec/api/portal1.x/ ~/.ec/api/conf
-wget -q --show-progress -O ~/.ec/api/portal1.x/v1.1beta.tar.gz https://github.com/EC-Release/web-ui/archive/v1.1beta.tar.gz
+
+# temp for PR https://github.com/EC-Release/web-ui/pull/112
+wget -q --show-progress -O ~/.ec/api/portal1.x/v1.1beta.tar.gz https://github.com/Subhash-vishwakarma/web-ui/archive/v1.1beta.zip
+#wget -q --show-progress -O ~/.ec/api/portal1.x/v1.1beta.tar.gz https://github.com/EC-Release/web-ui/archive/v1.1beta.tar.gz
+
 tar -xzf ~/.ec/api/portal1.x/v1.1beta.tar.gz --strip 1 -C ~/.ec/api/portal1.x
 rm ~/.ec/api/portal1.x/v1.1beta.tar.gz
 wget -q --show-progress -O ~/.ec/api/conf/api.yaml https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/conf/api.yaml
