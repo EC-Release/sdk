48e5b06d69d29fb7574ce47e8fb28b5e46ef69f7
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-06-09 13:43:22 -0700

    Create portal_conf.sh

---
+++ scripts/api/dcsc1.x/portal_conf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p ~/.ec/api/dcsc1.x/ ~/.ec/api/conf
+
+wget -q --show-progress -O ~/.ec/api/dcsc1.x/v1beta.tar.gz https://github.com/dc-release/dc-portal/archive/refs/heads/v1beta.zip
+
+tar -xzf ~/.ec/api/dcsc1.x/v1beta.tar.gz --strip 1 -C ~/.ec/api/dcsc1.x
+rm ~/.ec/api/dcsc1.x/v1beta.tar.gz
+wget -q --show-progress -O ~/.ec/api/conf/api.yaml https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/conf/api.yaml
+ls -al ~/.ec/api/dcsc1.x ~/.ec/api ~/.ec/api/conf/
