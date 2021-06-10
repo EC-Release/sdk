292b4c458ff17e1f8edfa6a11ff4ac2206e1e30e
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-06-10 00:27:24 -0700

    Update portal_conf.sh

--- scripts/api/dcsc1.x/portal_conf.sh
+++ scripts/api/dcsc1.x/portal_conf.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 mkdir -p ~/.ec/api/dcsc1.x/ ~/.ec/api/conf
 
-wget -q --show-progress -O ~/.ec/api/dcsc1.x/v1beta.tar.gz https://github.com/dc-release/dc-portal/archive/refs/heads/v1beta.tar.gz
+#temp commented out to benefit the development.
+#wget -q --show-progress -O ~/.ec/api/dcsc1.x/v1beta.tar.gz https://github.com/dc-release/dc-portal/archive/refs/heads/v1beta.tar.gz
+wget -q --show-progress -O ~/.ec/api/dcsc1.x/v1beta.tar.gz https://github.com/paskantishubham/dc-portal/archive/refs/heads/dev.tar.gz
 
 tar -xzf ~/.ec/api/dcsc1.x/v1beta.tar.gz --strip 1 -C ~/.ec/api/dcsc1.x
 rm ~/.ec/api/dcsc1.x/v1beta.tar.gz
