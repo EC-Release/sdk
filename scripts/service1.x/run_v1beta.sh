0c880a91d87e2254c20373d2791f32cfb3e47360
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-08-06 11:29:22 -0700

    Update run_v1beta.sh

--- scripts/service1.x/run_v1beta.sh
+++ scripts/service1.x/run_v1beta.sh
@@ -8,7 +8,7 @@ export EC_PUB_KEY="$(cat ./temp/service.cer)"
 export EC_PRVT_KEY="$(cat ./temp/service.key)"
 
 printf "begin test keypair"
-source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.2beta.linux64.txt) 
+source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64.txt) 
 
 if [[ -z "${EC_PPS}" ]]; then
   export EC_PPS=${EC_PRVT_ADM}  
