e214f592ae6e25e20337917b9647bec7addea435
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-08-25 13:13:03 -0700

    Update portal_conf.sh

--- scripts/api/dcsc1.x/portal_conf.sh
+++ scripts/api/dcsc1.x/portal_conf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p ~/.ec/api/dcsc1.x/ ~/.ec/api/conf
+mkdir -p ~/.ec/api/dcsc1.x/ ~/.ec/api/conf ~/.ec/tmp/portal
 
 #temp commented out to benefit the development.
 #wget -q --show-progress -O ~/.ec/api/dcsc1.x/v1beta.tar.gz https://github.com/dc-release/dc-portal/archive/refs/heads/v1beta.tar.gz
@@ -10,9 +10,17 @@ mkdir -p ~/.ec/api/dcsc1.x/ ~/.ec/api/conf
 #  wget -q --show-progress -O ~/.ec/api/dcsc1.x/v1beta.tar.gz https://github.com/paskantishubham/dc-portal/archive/refs/heads/dev.tar.gz
 #fi
 
+#swagger-api doc
+
+  wget -q --show-progress -O ~/.ec/tmp/portal/v1.2beta.tar.gz https://github.com/EC-Release/ng-portal/archive/refs/heads/v1.2beta.tar.gz
+
 tar -xzf ~/.ec/api/dcsc1.x/disty.tar.gz --strip 1 -C ~/.ec/api/dcsc1.x
+
+tar -xzf ~/.ec/tmp/portal/v1.2beta.tar.gz --strip 1 -C ~/.ec/tmp/portal
+
 #tar -xzf ~/.ec/api/dcsc1.x/disty.tar.gz -C ~/.ec/api/dcsc1.x
 mv ~/.ec/api/dcsc1.x ~/.ec/api/webui-assets
-rm -Rf ~/.ec/api/dcsc1.x
+mv ~/.ec/tmp/portal/swagger-ui ~/.ec/api/webui-assets/
+rm -Rf ~/.ec/api/dcsc1.x ~/.ec/tmp/portal
 wget -q --show-progress -O ~/.ec/api/conf/api.yaml https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/api/conf/api.yaml
 tree ~/.ec/api
