d41b26a1789f14fb0629fae96ebb20c4d12b7678
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-09-10 14:38:20 -0700

    Update run_v1.sh

--- scripts/service1.x/run_v1.sh
+++ scripts/service1.x/run_v1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir -p /root/temp
+mkdir -p ./temp
 wget -q --show-progress -O /root/temp/service.cer https://raw.githubusercontent.com/EC-Release/x509/main/crt-list/beta/c2211cb7-3ae6-4a8f-a6c4-01577615f318.cer?token=${GITHUB_TOKEN}
 wget -q --show-progress -O /root/temp/service.key https://raw.githubusercontent.com/EC-Release/pkeys/master/451ecf94-b442-4ebb-904e-0e1b50d8b1de.key?token=${GITHUB_TOKEN}
 wget -q --show-progress -O /root/temp/service.hash https://raw.githubusercontent.com/EC-Release/hash-list/main/service1.x.hash?token=${GITHUB_TOKEN}
-ls -al
+ls -al /root/temp
 exit 0
 
 ls -al
