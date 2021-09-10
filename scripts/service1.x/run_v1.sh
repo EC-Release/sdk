fcf74c6ed416353322f2789128ace241097460bf
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-09-10 16:33:56 -0700

    Update run_v1.sh

--- scripts/service1.x/run_v1.sh
+++ scripts/service1.x/run_v1.sh
@@ -41,25 +41,24 @@ rm -rf ec-px-service
 wget https://gitlab.com/ec-release/cf-service/-/archive/v1/cf-service-v1.tar.gz
 tar -xzf cf-service-v1.tar.gz
 mv cf-service-v1 ec-px-service
-cd ec-px-service && wget https://gitlab.com/ec-release/cf-service-assets/-/archive/v1/cf-service-assets-v1.tar.gz
-rm -rf assets/ && tar -xzf cf-service-assets-v1.tar.gz
-mv cf-service-assets-v1 assets && rm -rf cf-service-assets-v1.tar.gz 
-rm -rf ec-web-ui/
+cd ec-px-service
+
+wget https://gitlab.com/ec-release/cf-service-assets/-/archive/v1/cf-service-assets-v1.tar.gz
+tar -xzf cf-service-assets-v1.tar.gz
+mv cf-service-assets-v1 assets
 wget https://gitlab.com/ec-release/cf-service-webui/-/archive/v1/cf-service-webui-v1.tar.gz
 tar -xzf cf-service-webui-v1.tar.gz
 mv cf-service-webui-v1 ec-web-ui
-rm -rf cf-service-webui-v1.tar.gz
-#ls -la
-#ls -la assets/
-#ls -la ec-web-ui/ 
-cd ..
+
+rm cf-service-webui-v1.tar.gz cf-service-assets-v1.tar.gz ./../cf-service-v1.tar.gz
+
 printf "Service code downloaded successfully"
 
-cp ./temp/service.key ./ec-px-service
-cp ./temp/service.cer ./ec-px-service
+cp ./../temp/service.key ./
+cp ./../temp/service.cer ./
 
 printf "begin auth-api replacement"
-cd ./ec-px-service
+#cd ./ec-px-service
 wget -q --show-progress https://github.com/EC-Release/auth-api/raw/v1/dist/api/api_linux.tar.gz
 tar -xzf api_linux.tar.gz
 chmod +x api_linux
@@ -67,7 +66,7 @@ printf "end auth-api replacement"
 
 tree ./
 npm install
-ls -la $(pwd)
+#ls -la $(pwd)
 eval "sed -i -e 's#{HOST}#${APP}-${ENV}.${HOST}#g' ./assets/swagger.json"
 eval "sed -i -e 's#{BASE}#${EC_REV}#g' ./assets/swagger.json"
 eval "sed -i -e 's#{ENV}#${ENV}#g' ./assets/swagger.json"
