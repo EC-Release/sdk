0b74164c2633f501d6c8ac3ae2ee7e2ee1ad5428
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-09-10 15:38:42 -0700

    Update run_v1.sh

--- scripts/service1.x/run_v1.sh
+++ scripts/service1.x/run_v1.sh
@@ -25,7 +25,7 @@ printf "begin test keypair"
 #source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.2beta.linux64.txt) 
 
 #if [[ -z "${EC_PPS}" ]]; then
-export EC_PPS=${cat ./temp/service.hash}
+export EC_PPS=$(cat ./temp/service.hash)
 #fi
 export EC_PPS=$(agent -hsh -smp)
 
