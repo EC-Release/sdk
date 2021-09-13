4941d8d01ea156ba2e2857539d76b50307ff90dd
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-09-13 14:16:07 -0700

    Update find-problem-instances.sh

--- scripts/service1.x/cf-ops/find-problem-instances.sh
+++ scripts/service1.x/cf-ops/find-problem-instances.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-function getProblemInstances(){
+function hasEnvVar () {
+    hasIssue = false
+    while read line; do
+       # do some finding blah
+       if 
+         hasIssue = true
+         printf "\n instance (%s) has missing field: %s\n" "$1" "$line"
+       fi
+    done < field_list.txt
+    
+    if [[ -eq "$hasIssue" true ]]; then
+        echo $1 >> problemInsts.txt
+    fi 
+}
+
+function getProblemInstances () {
     #cf delete ${ZONE} -f 
     #cd ./push
     #cf push
     #cd -
     login
     echo "Login successful"
-    
+    printf "\ngetting the list..\n\n" 
     cf a | grep -E '0/|\?/'| awk '{print $1}'| while read -r line ; do
-      printf "\n$line\n"
+      printf "\nevaluating the app name: %s" "$line"
+      ev=$(cf env $line)
+      hasEnvVar "$line" "$ev"
     done
+    
+    echo "list the problem instances"
+    cat problem_insts.txt
 }
