a7ecf8f6fd9c9af407f84b3f91b639a3b306d629
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-09-13 13:17:42 -0700

    Update find-problem-instances.sh

--- scripts/service1.x/cf-ops/find-problem-instances.sh
+++ scripts/service1.x/cf-ops/find-problem-instances.sh
@@ -3,6 +3,12 @@
 function getProblemInstances(){
     #cf delete ${ZONE} -f 
     #cd ./push
-    cf push
+    #cf push
     #cd -
+    login
+    echo "Login successful"
+    
+    cf a | grep -E '0/|\?/'| awk '{print $1}'| while read -r line ; do
+      printf "\n$line\n"
+    done
 }
