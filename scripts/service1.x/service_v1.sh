c471ea47d2d9ce9c638abd35318f4a9511945510
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-09-13 17:41:45 -0700

    Update service_v1.sh

--- scripts/service1.x/service_v1.sh
+++ scripts/service1.x/service_v1.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+#  Copyright (c) 2020 General Electric Company. All rights reserved.
+#
+#  The copyright to the computer software herein is the property of
+#  General Electric Company. The software may be used and/or copied only
+#  with the written permission of General Electric Company or in accordance
+#  with the terms and conditions stipulated in the agreement/contract
+#  under which the software has been supplied.
+#
+#  author: apolo.yasuda@ge.com
+#
 
 #temp. pls remove this line in release
 #sleep 10
