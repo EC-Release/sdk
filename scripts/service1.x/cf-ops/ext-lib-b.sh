70246f6091757f21fb3056c8c74603c9f962a77f
ramaraosrikakulapu <38732583+ramaraosrikakulapu@users.noreply.github.com>
2021-09-16 19:16:59 -0400

    cert upgrade ext lib changes (#175)
    * cert upgrade - get routing details and marking
    
    * Update ext-lib-a.sh
    
    * token fix
    
    * ext lib b
    
    * updated ext lib b
    
    * added author to ext lib b
    
    * Update ext-lib-b.sh (#4)
    
    * Update ext-lib-b.sh
    
    * Update ext-lib-b.sh
    
    * Update find-problem-instances.sh
    
    Co-authored-by: A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>

--- scripts/service1.x/cf-ops/ext-lib-b.sh
+++ scripts/service1.x/cf-ops/ext-lib-b.sh
@@ -8,30 +8,50 @@
 #  with the terms and conditions stipulated in the agreement/contract
 #  under which the software has been supplied.
 #
+#  autor: puja.sharma@ge.com
 
 # find and output the original app belong to the given app $1
 # $1: <app name>
 function findInstOfOrigin () {
 
+  #theOrigInst=$(echo $1 | awk -F'-2022' '{print $1}')
+  theOrigInst=${1%-"$MISSION"}
+
+  cf app $theOrigInst > ~tmp 2>&1
+  getApp=$(cat ~tmp | grep -e 'FAILED')
+
+  if [[ ! -z $getApp ]]; then
+    printf "$1 (unknown instance)\n" >> ~unknownProcStep2Insts.txt
+    return
+  fi
+
+  #getEnv=$(cf e $theOrigInst | grep -e 'UPDATED: '$MISSION)
+  instStep1=$(hasEnvVar "$theOrigInst" 'UPDATED: '$MISSION)
+  #echo $instStep1
+  if [[ $instStep1 == "1" ]]; then
+    printf "$theOrigInst"
+    return
+  fi
+
   # if have some doubts
   printf "$1 (unknown instance)\n" >> ~unknownProcStep2Insts.txt
-  
-  # output the origin inst
-  printf "$theOrigInst"
 }
+
 
 # unmap the url route $2 from the app name $1
 # $1: <app name>
 # $2: <URL route>
 function unmapInstURL () {
-  
+  hostname=$(echo $2 | cut -d'.' -f 1)
+  cf unmap-route $1 run.aws-usw02-pr.ice.predix.io --hostname $hostname  
 }
 
 # mapping the url route $2 to the app name $1
 # $1: <app name>
 # $2: <URL route>
 function mapInstURL () {
-  
+  hostname=$(echo $2 | cut -d'.' -f 1)
+  cf map-route $1 run.aws-usw02-pr.ice.predix.io --hostname $hostname
 }
 
 # update the url routing from the give app name $1 to the new app name $2
