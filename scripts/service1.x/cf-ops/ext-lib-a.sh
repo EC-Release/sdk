7e4522e91e85b87b6ee0bf4d2bb8c3a8d747c5b6
ramaraosrikakulapu <38732583+ramaraosrikakulapu@users.noreply.github.com>
2021-09-16 16:31:55 -0400

    Update ext-lib-a.sh

--- scripts/service1.x/cf-ops/ext-lib-a.sh
+++ scripts/service1.x/cf-ops/ext-lib-a.sh
@@ -14,33 +14,23 @@
 # find the routing based on $1
 # $1: <app name>
 function findCurrentRouting () {
-
   {
     current_routes=$(cf app $1 | grep routes | awk -F':' '{print $2}' | xargs)
-
-    if [[ -z "$current_routes" ]]; then
-      printf "\n\nno routes for the instance %s \n", "$1"
-    else
-      printf "\n\n current routes for %s are: %s \n", "$1" "$current_routes"
+    
+    if [[ ! -z "$current_routes" ]]; then
+      printf "$current_routes"
     fi
-
-  } || {
-    printf "\n\nunable to get the routes for %s \n", "$1"
   }
-
-  # if have some doubts
-#  printf "$1 (unknown instance)\n" >> ~unknownProcStep2Insts.txt
-#
-#  printf "$theRouting"
 }
 
 # set the env var in the app $1 as the completion of step2
 # $1: <app name>
 function setStep2CompletedEnv () {
-  {
-    cf set-env $1 'UPDATED' '2022-DONE'
-    printf "\n\n marked %s as step2 completed \n", "$1"
-  } || {
-    printf "\n\n unable to mark %s as step2 completed \n", "$1"
-  }
+  result=$(cf set-env "$1" UPDATED $MISSION'-DONE')
+
+  if [[ $result != *"FAILED"* ]]; then
+    printf "0"
+    return
+  fi
+  printf "1"
 }
