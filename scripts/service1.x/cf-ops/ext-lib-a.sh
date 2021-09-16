3430b63491c623a2b3480b04714dff7c57416112
Rama Rao Srikakulapu <RamaRao.Srikakulapu@ge.com>
2021-09-16 15:37:37 -0400

    cert upgrade - get routing details and marking

--- scripts/service1.x/cf-ops/ext-lib-a.sh
+++ scripts/service1.x/cf-ops/ext-lib-a.sh
@@ -8,19 +8,39 @@
 #  with the terms and conditions stipulated in the agreement/contract
 #  under which the software has been supplied.
 #
+#  author: RamaRao.Srikakulapu@ge.com
+#
 
 # find the routing based on $1
 # $1: <app name>
 function findCurrentRouting () {
-  # if have some doubts
-  printf "$1 (unknown instance)\n" >> ~unknownProcStep2Insts.txt  
 
-  printf "$theRouting"
+  {
+    current_routes=$(cf app $1 | grep routes | awk -F':' '{print $2}' | xargs)
+
+    if [[ -z "$current_routes" ]]; then
+      printf "\n\nno routes for the instance %s \n", "$1"
+    else
+      printf "\n\n current routes for %s are: %s \n", "$1" "$current_routes"
+    fi
+
+  } || {
+    printf "\n\nunable to get the routes for %s \n", "$1"
+  }
+
+  # if have some doubts
+#  printf "$1 (unknown instance)\n" >> ~unknownProcStep2Insts.txt
+#
+#  printf "$theRouting"
 }
 
 # set the env var in the app $1 as the completion of step2
 # $1: <app name>
 function setStep2CompletedEnv () {
-
-
+  {
+    cf set-env $1 'UPDATED' '2022-DONE'
+    printf "\n\n marked %s as step2 completed \n", "$1"
+  } || {
+    printf "\n\n unable to mark %s as step2 completed \n", "$1"
+  }
 }
