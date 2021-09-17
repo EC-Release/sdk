3056549660c62dbb2b94f833ed7259998c4f6cc9
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-09-17 11:07:58 -0700

    Update ext-lib-b.sh

--- scripts/service1.x/cf-ops/ext-lib-b.sh
+++ scripts/service1.x/cf-ops/ext-lib-b.sh
@@ -58,7 +58,24 @@ function mapInstURL () {
 # $1: <original app name>
 # $2: <new app name>
 function updateInstURL () {
+
   theRouting=$(findCurrentRouting $1)
-  unmapInstURL $1 $theRouting
-  mapInstURL $2 $theRouting
+  if [[ -z $theRouting ]]; then
+    printf "empty routing."
+    return
+  fi
+  
+  ref=$(unmapInstURL $1 $theRouting | grep -e 'FAILED')
+  if [[ ! -z $ref ]]; then
+    printf "error in unmapping."
+    return
+  fi
+  
+  ref1=$(mapInstURL $2 $theRouting | grep -e 'FAILED')
+  if [[ ! -z $ref1 ]]; then
+    printf "error in re-mapping."
+    return
+  fi
+  
+  printf "0"
 }
