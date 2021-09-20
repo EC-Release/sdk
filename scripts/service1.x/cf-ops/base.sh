9ab9f124db97ff9cef65bf8b26da2641b3bb26ae
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-09-20 13:22:29 -0700

    Update base.sh

--- scripts/service1.x/cf-ops/base.sh
+++ scripts/service1.x/cf-ops/base.sh
@@ -150,10 +150,10 @@ function checkInLogger () {
   printf "\ncheck-in logs for the function %s\n" "$1"
 
   ref=$(strCamel2Dash "$1")
-  [[ -e "~$__DBG$1" ]] && cat "~$__DBG$1" >> ./logs/"$ref$__DBG_FIL_EXT$__LOG_FIL_EXT"
-  [[ -e "~$__PAS$1" ]] && cat "~$__PAS$1" >> ./logs/"$ref$__PAS_FIL_EXT$__LOG_FIL_EXT"
-  [[ -e "~$__ERR$1" ]] && cat "~$__ERR$1" >> ./logs/"$ref$__ERR_FIL_EXT$__LOG_FIL_EXT"
-  [[ -e "~$__UKN$1" ]] && cat "~$__UKN$1" >> ./logs/"$ref$__UKN_FIL_EXT$__LOG_FIL_EXT"
+  [[ -e "~$__DBG$1" ]] && mv "~$__DBG$1" ./logs/"$ref$__DBG_FIL_EXT$__LOG_FIL_EXT"
+  [[ -e "~$__PAS$1" ]] && mv "~$__PAS$1" ./logs/"$ref$__PAS_FIL_EXT$__LOG_FIL_EXT"
+  [[ -e "~$__ERR$1" ]] && mv "~$__ERR$1" ./logs/"$ref$__ERR_FIL_EXT$__LOG_FIL_EXT"
+  [[ -e "~$__UKN$1" ]] && mv "~$__UKN$1" ./logs/"$ref$__UKN_FIL_EXT$__LOG_FIL_EXT"
   
 }
 
