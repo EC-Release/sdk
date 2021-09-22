70cec6c1dbdd69db3ca00aaa0c64370b1c5217d6
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-09-21 22:31:35 -0700

    Update ext-lib-a.sh

--- scripts/service1.x/cf-ops/ext-lib-a.sh
+++ scripts/service1.x/cf-ops/ext-lib-a.sh
@@ -16,6 +16,7 @@
 function findCurrentRouting () {
     
     ref=$(findUUID "$1")
+    echo '$ref: '$ref    
     current_routes=$(cat $__CACHED_ALL_ROUTES | grep -e "$ref" | awk 'length($2)==36 {print $2"."$3}')
     
     if [[ ! -z "$current_routes" ]]; then
