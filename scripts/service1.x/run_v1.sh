fae1778b953a5ebeb5e7a4a8a0ff84d568a79f36
ramaraosrikakulapu <38732583+ramaraosrikakulapu@users.noreply.github.com>
2022-02-08 09:16:53 -0500

    Install package in service container
    These changes are required to migrate EC service to deploy in EKS containers. 
    - Idea is to save the env variables in ~/.env file and will be persisted with PV.
    - `dotenv` npm package helps to set environment variables in container
    - `touch` command helps to ensure the ~/.env file existence.

--- scripts/service1.x/run_v1.sh
+++ scripts/service1.x/run_v1.sh
@@ -123,6 +123,10 @@ eval "sed -i -e 's#{HOST}#${APP}-${ENV}.${HOST}#g' ./assets/swagger.json"
 eval "sed -i -e 's#{BASE}#${EC_REV}#g' ./assets/swagger.json"
 eval "sed -i -e 's#{ENV}#${ENV}#g' ./assets/swagger.json"
 eval "sed -i -e 's#{APP_PATH}#/${REV}/index#g' ./assets/index.html"
+
+touch -a ~/.env
+npm install dotenv
+
 node ./app.js
 
 
