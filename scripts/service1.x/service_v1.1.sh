ff101d1ce74cb9cbad5e61b7c44bd5b59169f44a
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-06-16 18:04:59 -0700

    Update service_v1.1.sh

--- scripts/service1.x/service_v1.1.sh
+++ scripts/service1.x/service_v1.1.sh
@@ -70,7 +70,7 @@ EC_SCRIPT_2=$(echo "$ownrInf" | jq -r '.SCRIPT_2') \
 EC_SCRIPT_3=$(echo "$ownrInf" | jq -r '.SCRIPT_3')
 
 ref=$(getURLHostnameAndPort "$EC_SVC_URL")
-op=$(echo "$ownrInf" | jq -r 'any(.SVC_LIST["'$EC_SVC_ID'"]; . == "'$ref'")')
+op=$(echo "$ownrInf" | jq -r 'any(.SVC_LIST["'$EC_SVC_ID'"][]; . == "'$ref'")')
 if [ "$op" != "true" ]; then
   echo "      |_ [!] unauthorised svc (id: ${EC_SVC_ID}; url: ${EC_SVC_URL}) deployment."  
   echo $ownrInf | jq -r '.SVC_LIST["'$EC_SVC_ID'"]'
